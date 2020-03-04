d64794d33d0dd079890d11d552c6584c4f390ace
Wensenter <18365206169@163.com>
2020-03-04 18:07:42 +0800

    complate index.html

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
+// 禁止页面滑动
+var overscroll = function(el) {
+  el.addEventListener('touchstart', function() {
+    var top = el.scrollTop
+      , totalScroll = el.scrollHeight
+      , currentScroll = top + el.offsetHeight;
+    if(top === 0) {
+      el.scrollTop = 1;
+    } else if(currentScroll === totalScroll) {
+      el.scrollTop = top - 1;
+    }
+  },{passive: false});
+  el.addEventListener('touchmove', function(evt) {
+    if(el.offsetHeight < el.scrollHeight)
+      evt._isScroller = true;
+  },{passive: false});
+}
+overscroll(document.querySelector('.scroll'));
+//带有scroll类名的可以滑动
+document.body.addEventListener('touchmove', function(evt) {
+  if(!evt._isScroller) {
+    evt.preventDefault();
+  }
+},{passive: false});
 // swiper
 var mySwiper = new Swiper('.swiper-container',{
     autoplay:true,
@@ -5,4 +29,4 @@ var mySwiper = new Swiper('.swiper-container',{
     pagination: {
         el: '.swiper-pagination',
       },
-    })
+})
